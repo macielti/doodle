d2481b0665f244000a13f8ab5291f565a1aa4f6e
Bruno do Nascimento Maciel <brunodonascimentomaciel@gmail.com>
2021-09-04 03:25:34 -0300

    the initial veriosn of send notification via email is working 😃

---
+++ src/microservice_notification/adapters/send_grid/notification.clj
@@ -0,0 +1,13 @@
+(ns microservice-notification.adapters.send-grid.notification
+  (:require [schema.core :as s]
+            [microservice-notification.wire.in.notification :as wire.in.notification]))
+
+(s/defn ->wire
+  [{:keys [value]} :- wire.in.notification/NotificationMessage
+   company-email :- s/Str
+   send-grid-key :- s/Str]
+  {:api-token (str "Bearer " send-grid-key)
+   :from      company-email
+   :to        (:email value)
+   :subject   (:title value)
+   :message   (:content value)})
--- src/microservice_notification/diplomatic/consumer.clj
+++ src/microservice_notification/diplomatic/consumer.clj
@@ -1,15 +1,20 @@
 (ns microservice-notification.diplomatic.consumer
   (:use [clojure pprint])
   (:require [microservice-notification.wire.in.notification :as wire.in.notification]
+            [microservice-notification.adapters.send-grid.notification :as adapters.send-grid.notification]
             [clojure.tools.logging :as log]
-            [sendgrid.core :as sg]))
+            [sendgrid.core :as sg]
+            [schema.core :as s]))
 
-(defn send-notification!
-  [{:keys [topic value] :as message} :- wire.in.notification/NotificationMessage
-   {:keys []} :config]
-  (sg/send-email)
+(s/defn send-notification!
+  [{:keys [value] :as message} :- wire.in.notification/NotificationMessage
+   {{:keys [send-grid-key company-email]} :config}]
+  (s/validate wire.in.notification/NotificationMessage message)
+  (sg/send-email (adapters.send-grid.notification/->wire message
+                                                         company-email
+                                                         send-grid-key))
   (log/info message))
 
 (def topic-consumers
-  {:notification {:schema  nil
+  {:notification {:schema  wire.in.notification/NotificationMessage
                   :handler send-notification!}})
