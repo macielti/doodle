addec5a490fc5df497a3a7d5f860ef5aff019467
Bruno do Nascimento Maciel <brunodonascimentomaciel@gmail.com>
2021-09-04 18:52:08 -0300

    logg config file path

--- src/microservice_notification/config.clj
+++ src/microservice_notification/config.clj
@@ -3,15 +3,17 @@
   (:require [microservice-notification.adapters.common :as adapters.common]
             [com.stuartsierra.component :as component]
             [cheshire.core :as json]
-            [environ.core :as environ]))
+            [environ.core :as environ]
+            [clojure.tools.logging :as log]))
 
 (defrecord Config []
   component/Lifecycle
   (start [this]
-    (let [config (json/parse-string (slurp (or (environ/env :clj-config-path) "resources/config.json"))
-                                    adapters.common/str->keyword-kebab-case)]
+    (let [config-path (or (environ/env :clj-config-path) "resources/config.json")
+          config      (json/parse-string (slurp config-path)
+                                         adapters.common/str->keyword-kebab-case)]
+      (log/info :config-path config-path)
       (assoc this :config config)))
-
   (stop [this]
     (assoc this :config nil)))
 
