e14d23143c175f3051b32cb983105092f8f34f08
Bruno do Nascimento Maciel <brunodonascimentomaciel@gmail.com>
2021-09-05 07:59:55 -0300

    update notification consume message schema

--- src/microservice_notification/wire/in/notification.clj
+++ src/microservice_notification/wire/in/notification.clj
@@ -1,8 +1,10 @@
 (ns microservice-notification.wire.in.notification
   (:require [schema.core :as s]))
 
+;TODO: Try to use a loose schema here
 (s/defschema NotificationMessage
   {:topic (s/enum :notification)
-   :value {:email   s/Str
-           :title   s/Str
-           :content s/Str}})
+   :value {:email             s/Str
+           :password-reset-id s/Str
+           :title             s/Str
+           :content           s/Str}})
