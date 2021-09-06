5b575a7f5904cf2af7882d74c4ecd3e15490dcba
Bruno do Nascimento Maciel <brunodonascimentomaciel@gmail.com>
2021-09-06 11:55:58 -0300

    use loose schema on incoming messages

--- src/microservice_notification/diplomatic/consumer.clj
+++ src/microservice_notification/diplomatic/consumer.clj
@@ -1,15 +1,17 @@
 (ns microservice-notification.diplomatic.consumer
-  (:use [clojure pprint])
   (:require [microservice-notification.wire.in.notification :as wire.in.notification]
             [microservice-notification.adapters.send-grid.notification :as adapters.send-grid.notification]
             [clojure.tools.logging :as log]
             [sendgrid.core :as sg]
-            [schema.core :as s]))
+            [schema.core :as s])
+  (:import (clojure.lang ExceptionInfo)))
 
 (s/defn send-notification!
   [{:keys [value] :as message} :- wire.in.notification/NotificationMessage
    {{:keys [send-grid-key company-email]} :config}]
-  (s/validate wire.in.notification/NotificationMessage message)
+  (try (s/validate wire.in.notification/NotificationMessage message)
+       (catch ExceptionInfo e
+         (log/error e)))
   (let [result (sg/send-email (adapters.send-grid.notification/->wire message
                                                                       company-email
                                                                       send-grid-key))]
--- src/microservice_notification/wire/in/notification.clj
+++ src/microservice_notification/wire/in/notification.clj
@@ -1,10 +1,13 @@
 (ns microservice-notification.wire.in.notification
-  (:require [schema.core :as s]))
+  (:require [schema.core :as s]
+            [common-clj.schema.core :as schema]))
 
-;TODO: Try to use a loose schema here
-(s/defschema NotificationMessage
+;TODO: verify is the skeleton pattern naming is used only for datomic entities
+(def notification-message-skeleton
   {:topic (s/enum :notification)
-   :value {:email             s/Str
-           :password-reset-id s/Str
-           :title             s/Str
-           :content           s/Str}})
+   :value {:email   s/Str
+           :title   s/Str
+           :content s/Str}})
+
+(s/defschema NotificationMessage
+  (schema/loose-schema notification-message-skeleton))
