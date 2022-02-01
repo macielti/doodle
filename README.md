4285b67cb91bb6229f38507ba99d2973a9898319
Bruno do Nascimento Maciel <brunodonascimentomaciel@gmail.com>
2022-02-01 19:32:01 -0300

    better service description

--- README.md
+++ README.md
@@ -2,4 +2,5 @@
 
 ![Doodle](https://user-images.githubusercontent.com/13934379/151779899-fe6c5a15-3345-43a4-9a22-fb08dc65e8fe.png)
 
-Doodle levava mensagens secretas da resistência para os aliados durante a I Guerra Mundial
+Doodle carried secret messages from the resistance to the allies during World War I.
+This service is about customer notification like by sending emails.
--- src/doodle/components.clj
+++ src/doodle/components.clj
@@ -9,5 +9,10 @@
     :config (component.config/new-config "resources/config.json" :prod)
     :consumer (component/using (component.consumer/new-consumer diplomatic.consumer/topic-consumers) [:config])))
 
+(def system-test
+  (component/system-map
+    :config (component.config/new-config "resources/config.json" :test)
+    :consumer (component/using (component.consumer/new-consumer diplomatic.consumer/topic-consumers) [:config])))
+
 (defn start-system! []
   (component/start system))
