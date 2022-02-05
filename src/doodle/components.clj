ffc4aca342b72443150b125027afec1912f7da20
Bruno do Nascimento Maciel <brunodonascimentomaciel@gmail.com>
2022-02-05 16:50:21 -0300

    fix integration tests

--- src/doodle/components.clj
+++ src/doodle/components.clj
@@ -1,18 +1,20 @@
 (ns doodle.components
   (:require [com.stuartsierra.component :as component]
             [common-clj.component.config :as component.config]
-            [common-clj.component.kafka.consumer :as component.consumer]
+            [common-clj.component.kafka.producer :as component.kafka.producer]
+            [common-clj.component.kafka.consumer :as component.kafka.consumer]
             [doodle.diplomatic.consumer :as diplomatic.consumer]))
 
 (def system
   (component/system-map
     :config (component.config/new-config "resources/config.json" :prod)
-    :consumer (component/using (component.consumer/new-consumer diplomatic.consumer/topic-consumers) [:config])))
+    :consumer (component/using (component.kafka.consumer/new-consumer diplomatic.consumer/topic-consumers) [:config])))
 
 (def system-test
   (component/system-map
     :config (component.config/new-config "resources/config.example.json" :test)
-    :consumer (component/using (component.consumer/new-consumer diplomatic.consumer/topic-consumers) [:config])))
+    :consumer (component/using (component.kafka.consumer/new-mock-consumer diplomatic.consumer/topic-consumers) [:config])
+    :producer (component/using (component.kafka.producer/new-mock-producer) [:config :consumer])))
 
 (defn start-system! []
   (component/start system))
--- src/doodle/diplomatic/consumer.clj
+++ src/doodle/diplomatic/consumer.clj
@@ -1,9 +1,9 @@
 (ns doodle.diplomatic.consumer
   (:require [schema.core :as s]
             [sendgrid.core :as sg]
-            [clojure.tools.logging :as log]
             [doodle.wire.in.notification :as wire.in.notification]
-            [doodle.adapters.send-grid.notification :as adapters.send-grid.notification])
+            [doodle.adapters.send-grid.notification :as adapters.send-grid.notification]
+            [taoensso.timbre :as timbre])
   (:import (clojure.lang ExceptionInfo)))
 
 (s/defn send-notification!
@@ -11,11 +11,11 @@
    {{:keys [send-grid-key company-email]} :config}]
   (try (s/validate wire.in.notification/NotificationDocument document)
        (catch ExceptionInfo e
-         (log/error e)))
+         (timbre/error e)))
   (let [result (sg/send-email (adapters.send-grid.notification/->wire document
                                                                       company-email
                                                                       send-grid-key))]
-    (log/info document result)))
+    (timbre/info document result)))
 
 (def topic-consumers
   {:notification {:schema  wire.in.notification/NotificationDocument
