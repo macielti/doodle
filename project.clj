10c8222aceb6bc40dabc74884a6f16f62f451d27
Bruno do Nascimento Maciel <brunodonascimentomaciel@gmail.com>
2022-09-23 19:09:37 -0300

    upgrading dependencies

--- project.clj
+++ project.clj
@@ -3,21 +3,21 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url  "https://www.eclipse.org/legal/epl-2.0/"}
 
-  :plugins [[lein-cloverage "1.2.2"]
+  :plugins [[lein-cloverage "1.2.3"]
             [lein-environ "1.2.0"]]
 
-  :dependencies [[org.clojure/clojure "1.10.3"]
+  :dependencies [[org.clojure/clojure "1.11.1"]
                  [danlentz/clj-uuid "0.1.9"]
-                 [net.clojars.macielti/common-clj "9.13.9"]
-                 [ch.qos.logback/logback-classic "1.2.10"]
+                 [net.clojars.macielti/common-clj "17.22.19"]
+                 [ch.qos.logback/logback-classic "1.4.1"]
                  [org.apache.kafka/kafka-clients "2.8.0"]
                  [io.pedestal/pedestal.service "0.5.10"]
-                 [com.stuartsierra/component "1.0.0"]
-                 [camel-snake-kebab "0.4.2"]
-                 [prismatic/schema "1.2.0"]
+                 [com.stuartsierra/component "1.1.0"]
+                 [camel-snake-kebab "0.4.3"]
+                 [prismatic/schema "1.4.0"]
                  [clj-http-fake "1.0.3"]
                  [clj-sendgrid "0.1.2"]
-                 [cheshire "5.10.2"]
+                 [cheshire "5.11.0"]
                  [environ "1.2.0"]]
 
   :profiles {:test {:env {:clj-env         "test"
